007baa99f12df76cdda0bdd1e8dc2b815185d621
Angel Sullon <asullom@gmail.com>
2014-07-27 09:47:00 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var servidor= require('socket.io').listen(9000);
+
+servidor.sockets.on('connection', arranque);
+
+function arranque(autor){
+ 	console.log("arrancado\n");
+ 	autor.on('escucharMensaje', emitir); //escucharMensaje del autor
+}
+
+function emitir(mensaje){
+	console.log(mensaje+"\n");
+	servidor.sockets.emit('notificarMensaje',mensaje+"*"); //notificarMensaje los usuarios
+}
+
+console.log("Express server running at\n "+ 
+			"=> http://localhost:9000/\nCTRL + C to shutdown");
